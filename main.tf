436766da48f7cbfb964a291d008c40990275a31d
infradev4 <82362516+infradev4@users.noreply.github.com>
2022-01-12 23:01:35 +0100

    modify main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "sg_creation" {
-    source = "../modules/sg"
+    source = "./modules/sg"
     sg_author = var.author
     sg_env = var.env
 }
 
 module "ec2_creation" {
-    source = "../modules/ec2"
+    source = "./modules/ec2"
     ec2_instance_type = var.instance_type
     ec2_ami = data.aws_ami.myami.id
     ec2_key_name = var.key_name
